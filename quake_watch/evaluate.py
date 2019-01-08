c6ed3be9a0b05230397e91b5aa3b15dff3304215
calee14 <caplee1000@gmail.com>
2019-01-07 21:33:41 -0800

    trying to predict with model

--- quake_watch/evaluate.py
+++ quake_watch/evaluate.py
@@ -1,5 +1,8 @@
 from tensorflow.keras.models import load_model
 import pickle
+import datetime
+import time
+import numpy as np
 
 # Getting back the objects:
 with open('X_test.pkl', 'rb') as f:
@@ -10,3 +13,11 @@ with open('y_test.pkl', 'rb') as f:
 model = load_model('earthquake.h5')
 [test_loss, test_acc] = model.evaluate(X_test, y_test)
 print("Evaluation result on Test Data : Loss = {}, accuracy = {}".format(test_loss, test_acc))
+
+date = '10/17/1989 0:0:0'
+ts = datetime.datetime.strptime(date, '%m/%d/%Y %H:%M:%S')
+timestamp = time.mktime(ts.timetuple())
+a = np.array([timestamp, 37.7749, 122.4194])
+print(a)
+print(a.shape)
+model.predict(np.array([timestamp, 37.7749, 122.4194]))
