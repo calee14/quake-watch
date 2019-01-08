21d53cdc3a98e33ac828143a6639ab830ebfeeac
calee14 <caplee1000@gmail.com>
2019-01-07 20:36:14 -0800

    evaluating the model and checking how well it performs on test data

---
+++ quake_watch/evaluate.py
@@ -0,0 +1,12 @@
+from tensorflow.keras.models import load_model
+import pickle
+
+# Getting back the objects:
+with open('X_test.pkl', 'rb') as f:
+    X_test = pickle.load(f)
+with open('y_test.pkl', 'rb') as f:
+    y_test = pickle.load(f)
+
+model = load_model('earthquake.h5')
+[test_loss, test_acc] = model.evaluate(X_test, y_test)
+print("Evaluation result on Test Data : Loss = {}, accuracy = {}".format(test_loss, test_acc))
