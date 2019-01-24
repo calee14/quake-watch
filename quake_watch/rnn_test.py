f8ea2bdb6bdac63d453fbc16dd7c75bf6198a655
calee14 <caplee1000@gmail.com>
2019-01-24 12:29:14 -0800

    adding new model

--- quake_watch/rnn_test.py
+++ quake_watch/rnn_test.py
@@ -8,6 +8,9 @@ from sklearn.metrics import roc_auc_score
 
 import tensorflow.keras
 import tensorflow.keras.backend as K
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import LSTM
 
 def load_earthquake_data(path='data/'):
     csv_path = os.path.join(path, 'database.csv')
@@ -15,3 +18,48 @@ def load_earthquake_data(path='data/'):
 
 train_df = load_earthquake_data()
 print(train_df.shape)
+
+# get the features we want to pass into the model
+train_df = train_df[['Date', 'Time', 'Latitude', 'Longitude', 'Depth', 'Magnitude']]
+
+import datetime
+import time
+
+timestamp = []
+for d, t in zip(train_df['Date'], train_df['Time']):
+    try:
+        # changing the date time features into numeric values
+        ts = datetime.datetime.strptime(d+' '+t, '%m/%d/%Y %H:%M:%S')
+        timestamp.append(time.mktime(ts.timetuple()))
+    except:
+        # print('ValueError')
+        timestamp.append('ValueError')
+
+timeStamp = pd.Series(timestamp)
+
+# add the timestamp values into the feature set
+train_df['Timestamp'] = timeStamp.values
+final_data = train_df.drop(['Date', 'Time'], axis=1)
+final_data = final_data[final_data.Timestamp != 'ValueError']
+
+X = final_data[['Timestamp', 'Latitude', 'Longitude']]
+y = final_data[['Magnitude', 'Depth']]
+
+# split the data into a training set and testing set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+model = Sequential()
+model.reset_states()
+
+model.add(LSTM(input_dim=2, output_dim=100, input_shape=(3,), return_sequences=True))
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='softmax'))
+
+model.compile(optimizer='SGD', loss='squared_hinge', metrics=['accuracy'])
+
+model.fit(X_train, y_train, batch_size=10, epochs=10, verbose=1, validation_data=(X_test, y_test))
+
+[test_loss, test_acc] = model.evaluate(X_test, y_test)
+print("Evaluation result on Test Data : Loss = {}, accuracy = {}".format(test_loss, test_acc))
+
