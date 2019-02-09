36754e16a86c436e7d51545d4ea8dad8630651b1
calee14 <caplee1000@gmail.com>
2019-02-09 10:41:43 -0800

    reshaping data to fit in model

--- quake_watch/evaluate.py
+++ quake_watch/evaluate.py
@@ -12,19 +12,22 @@ with open('y_test.pkl', 'rb') as f:
     y_test = pickle.load(f)
 
 X_test = np.load('X_test.npy')
-y_test = np.load('n_test.npy')
+y_test = np.load('y_test.npy')
 
 model = load_model('eq.h5')
-model.load_weights('eq_weights.h5')
+model.reset_states()
+# model.load_weights('eq_weights.h5')
 [test_loss, test_acc] = model.evaluate(X_test, y_test)
 print("Evaluation result on Test Data : Loss = {}, accuracy = {}".format(test_loss, test_acc))
 
-date = '10/17/2019 5:4:15'
+date = '2/17/2019 5:4:15'
 ts = datetime.datetime.strptime(date, '%m/%d/%Y %H:%M:%S')
 timestamp = time.mktime(ts.timetuple())
 columns = ['Timestamp', 'Latitude', 'Longitude']
-data = np.array([[timestamp, 37.7749, 122.4194]]).T
+data = np.array([[timestamp, 37.7749, 122.4194]])
+data = np.reshape(data, (data.shape[0], data.shape[1], 1))
+print(data)
 # evaluate_data = pd.DataFrame(data, columns=columns)
 # final_data = evaluate_data[['Timestamp', 'Latitude', 'Longitude']]
-results = model.predict(X_test)
+results = model.predict(data)
 print(results)
