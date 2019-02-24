0a05ad99eef40c4603f0a891e49a321e5971a198
calee14 <caplee1000@gmail.com>
2019-02-24 15:48:17 -0800

    removing rnn and replacing with a simple regresoor

--- quake_watch/train.py
+++ quake_watch/train.py
@@ -1,5 +1,5 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense
 
 import pickle
 # Getting the training data
@@ -13,57 +13,22 @@ with open('X_test.pkl', 'rb') as f:
 with open('y_test.pkl', 'rb') as f:
     y_test = pickle.load(f)
 
-'''
-def create_model(neurons, activation, optimizer, loss):
-    model = Sequential()
-    model.add(Dense(neurons, activation=activation, input_shape=(3,)))
-    model.add(Dense(neurons, activation=activation))
-    model.add(Dense(2, activation='softmax'))
+from sklearn.ensemble import RandomForestRegressor
 
-    model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+reg = RandomForestRegressor(random_state=42)
+reg.fit(X_train, y_train)
+print(y_train)
+print(reg.predict(X_test))
 
-    return model
+print(reg.score(X_test, y_test))
 
-from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+# from sklearn.model_selection import GridSearchCV
 
-model = KerasClassifier(build_fn=create_model, verbose=0)
+# parameters = {'n_estimators':[10, 20, 50, 100, 200, 500]}
 
-neurons = [16, 64, 128, 256]
-batch_size = [10, 20, 50, 100]
-epochs = [10]
-activation = ['relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear', 'exponential']
-optimizer = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
-loss = ['squared_hinge']
+# grid_obj = GridSearchCV(reg, parameters)
+# grid_fit = grid_obj.fit(X_train, y_train)
+# best_fit = grid_fit.best_estimator_
+# best_fit.predict(X_test)
 
-param_grid = dict(neurons=neurons, batch_size=batch_size, epochs=epochs, activation=activation, optimizer=optimizer, loss=loss)
-
-from sklearn.model_selection import GridSearchCV
-
-# use the GridSearchCV module to find the best params for the model
-grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1)
-grid_result = grid.fit(X_train, y_train)
-
-print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-means = grid_result.cv_results_['mean_test_score']
-stds = grid_result.cv_results_['std_test_score']
-params = grid_result.cv_results_['params']
-# print out the best params
-for mean, stdev, param in zip(means, stds, params):
-    print("%f (%f) with: %r" % (mean, stdev, param))
-'''
-model = Sequential()
-model.add(Dense(16, activation='relu', input_shape=(3,)))
-model.add(Dense(16, activation='relu'))
-model.add(Dense(2, activation='softmax'))
-
-model.compile(optimizer='SGD', loss='squared_hinge', metrics=['accuracy'])
-
-model.fit(X_train, y_train, batch_size=10, epochs=20, verbose=1, validation_data=(X_test, y_test))
-
-[test_loss, test_acc] = model.evaluate(X_test, y_test)
-print("Evaluation result on Test Data : Loss = {}, accuracy = {}".format(test_loss, test_acc))
-
-model.save('earthquake.h5')
-
-results = model.predict(X_test)
-print(results)
+# best_fit.score(X_test, y_test)
