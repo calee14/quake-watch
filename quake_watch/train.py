6823b005aa62fefbbb340eb9fe374f8c2e214bc0
calee14 <caplee1000@gmail.com>
2019-02-24 18:22:56 -0800

    finding mean squared error and saving regressor to file

--- quake_watch/train.py
+++ quake_watch/train.py
@@ -1,6 +1,6 @@
 # from tensorflow.keras.models import Sequential
 # from tensorflow.keras.layers import Dense
-
+import numpy as np
 import pickle
 # Getting the training data
 with open('X_train.pkl', 'rb') as f:
@@ -13,6 +13,7 @@ with open('X_test.pkl', 'rb') as f:
 with open('y_test.pkl', 'rb') as f:
     y_test = pickle.load(f)
 
+# training and finding accuracy
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.externals import joblib
 
@@ -21,18 +22,25 @@ reg.fit(X_train, y_train)
 
 print(reg.score(X_test, y_test))
 
+# finding mean squared error
+from sklearn.metrics import mean_squared_error
+y_pred = reg.predict(X_test)
+reg_mse = mean_squared_error(y_test, y_pred)
+reg_rmse = np.sqrt(reg_mse)
+print(reg_rmse)
+
 # save the model to disk
 filename = 'finalized_model.sav'
-joblib.dump(model, filename)
+joblib.dump(reg, filename)
 
-from sklearn.model_selection import GridSearchCV
+# from sklearn.model_selection import GridSearchCV
 
-parameters = {'n_estimators':[10, 20, 50, 100, 200, 500]}
+# parameters = {'n_estimators':[10, 20, 50, 100, 200, 500]}
 
-grid_obj = GridSearchCV(reg, parameters)
-grid_fit = grid_obj.fit(X_train, y_train)
-best_fit = grid_fit.best_estimator_
-print(best_fit)
-best_fit.predict(X_test)
+# grid_obj = GridSearchCV(reg, parameters)
+# grid_fit = grid_obj.fit(X_train, y_train)
+# best_fit = grid_fit.best_estimator_
+# print(best_fit)
+# best_fit.predict(X_test)
 
-best_fit.score(X_test, y_test)
+# best_fit.score(X_test, y_test)
